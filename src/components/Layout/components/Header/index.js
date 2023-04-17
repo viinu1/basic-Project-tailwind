d6b13ac227b8ba1a804251b6c6a4afda85a71c52
Vĩ Phạm Văn <phamviabc2@gmail.com>
2023-04-17 13:17:31 +0700

    basic project

---
+++ src/components/Layout/components/Header/index.js
@@ -0,0 +1,9 @@
+function Header() {
+    return (
+        <div className="container mx-auto flex">
+            <div className="text-red-900 text-48">Header</div>
+        </div>
+    );
+}
+
+export default Header;
---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ['./src/**/*.{html,js}'],
+    theme: {
+        fontSize: {
+            16: '1.6rem',
+            20: '2rem',
+            32: '3.2rem',
+            48: '4.8rem',
+        },
+        extend: {},
+    },
+    plugins: [],
+};
