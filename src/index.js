e271f638e0a4196095b08fcb7f4c1152cea9dfe0
Vĩ Phạm Văn <phamviabc2@gmail.com>
2023-04-25 11:59:34 +0700

    complete

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom/client';
 
 import App from './App';
 import GlobalStyle from './components/GlobalStyles/GlobalStyle';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
